9e4c2036b6f66c5cc498ce2a2d4f62cf1e526d4a
LrVanAnhDuc <levananhduc1804@gmail.com>
2025-10-02 17:46:05 +0700

    feat: add content by locale

--- src/views/Signup/components/ButtonLogin/index.tsx
+++ src/views/Signup/components/ButtonLogin/index.tsx
@@ -1,19 +1,25 @@
+// libs
+import { useTranslations } from "next-intl";
 // others
 import CONSTANTS from "@/constants";
 import { Link } from "@/i18n/navigation";
 
 const { LOGIN } = CONSTANTS.ROUTES;
 
-const ButtonLogin = () => (
-  <main className="mt-4 text-center text-sm">
-    Already have an account?{" "}
-    <Link
-      href={LOGIN}
-      className="font-medium underline-offset-4 hover:underline"
-    >
-      Login
-    </Link>
-  </main>
-);
+const ButtonLogin = () => {
+  const t = useTranslations("signup.link");
+
+  return (
+    <main className="mt-4 text-center text-sm">
+      {t("descriptionSignUp")}?{" "}
+      <Link
+        href={LOGIN}
+        className="font-medium underline-offset-4 hover:underline"
+      >
+        {t("login")}
+      </Link>
+    </main>
+  );
+};
 
 export default ButtonLogin;
--- src/views/Signup/mains/SignupBlock/index.tsx
+++ src/views/Signup/mains/SignupBlock/index.tsx
@@ -1,3 +1,5 @@
+// libs
+import { useTranslations } from "next-intl";
 // components
 import {
   Card,
@@ -8,18 +10,22 @@ import {
 } from "@/components/ui/card";
 import FormSignup from "../../components/FormSignup";
 
-const SignupBlock = () => (
-  <main className="flex h-full items-center justify-center">
-    <Card className="w-sm">
-      <CardHeader>
-        <CardTitle>Create Your Account</CardTitle>
-        <CardDescription>Create an account to get started</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <FormSignup />
-      </CardContent>
-    </Card>
-  </main>
-);
+const SignupBlock = () => {
+  const t = useTranslations("signup.form");
+
+  return (
+    <main className="flex h-full items-center justify-center">
+      <Card className="w-sm">
+        <CardHeader>
+          <CardTitle>{t("title")}</CardTitle>
+          <CardDescription>{t("description")}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <FormSignup />
+        </CardContent>
+      </Card>
+    </main>
+  );
+};
 
 export default SignupBlock;
