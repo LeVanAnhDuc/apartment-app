8f9c61a17445e13bfef62a36971f8c5369e8d901
LrVanAnhDuc <levananhduc1804@gmail.com>
2025-12-21 10:26:29 +0700

    feat(Animated): add reusable animation wrapper components
    - FadeSlideUp: slide up + fade entrance animation
    - FadeIn: simple fade with optional y offset
    - ScaleSpring: spring-based scale pop-in effect
    - PulseRipple: infinite pulse rings for success states

---
+++ src/components/Animated/index.tsx
@@ -0,0 +1,4 @@
+export { default as FadeSlideUp } from "./FadeSlideUp";
+export { default as FadeIn } from "./FadeIn";
+export { default as ScaleSpring } from "./ScaleSpring";
+export { default as PulseRipple } from "./PulseRipple";
